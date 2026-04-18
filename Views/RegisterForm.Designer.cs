b1ca10b0a7f3845640735601d0b4ae70c251834a
TrungGaa4 <trannguyenquoctrung1234566789@gmail.com>
2026-04-18 15:02:06 +0700

    Thêm tính năng Register

---
+++ Views/RegisterForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace QuanLyKhachSan.Views
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            txtConfirmPassword = new TextBox();
+            txtEmail = new TextBox();
+            btnRegister = new Button();
+            linkLogin = new LinkLabel();
+            SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(297, 162);
+            txtUsername.Name = "txtUsername";
+            txtUsername.PlaceholderText = "Name";
+            txtUsername.Size = new Size(125, 27);
+            txtUsername.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(297, 195);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PlaceholderText = "Password";
+            txtPassword.Size = new Size(125, 27);
+            txtPassword.TabIndex = 1;
+            txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtConfirmPassword
+            // 
+            txtConfirmPassword.Location = new Point(297, 228);
+            txtConfirmPassword.Name = "txtConfirmPassword";
+            txtConfirmPassword.PlaceholderText = "ConfirmPassword";
+            txtConfirmPassword.Size = new Size(125, 27);
+            txtConfirmPassword.TabIndex = 2;
+            txtConfirmPassword.UseSystemPasswordChar = true;
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(297, 261);
+            txtEmail.Name = "txtEmail";
+            txtEmail.PlaceholderText = "Email";
+            txtEmail.Size = new Size(125, 27);
+            txtEmail.TabIndex = 3;
+            // 
+            // btnRegister
+            // 
+            btnRegister.Location = new Point(312, 294);
+            btnRegister.Name = "btnRegister";
+            btnRegister.Size = new Size(94, 29);
+            btnRegister.TabIndex = 4;
+            btnRegister.Text = "Đăng ký";
+            btnRegister.UseVisualStyleBackColor = true;
+            btnRegister.Click += btnRegister_Click;
+            // 
+            // linkLogin
+            // 
+            linkLogin.AutoSize = true;
+            linkLogin.Location = new Point(248, 338);
+            linkLogin.Name = "linkLogin";
+            linkLogin.Size = new Size(233, 20);
+            linkLogin.TabIndex = 5;
+            linkLogin.TabStop = true;
+            linkLogin.Text = "Đã có tài khoản? Đăng nhập ngay";
+            linkLogin.LinkClicked += linkLogin_LinkClicked;
+            // 
+            // RegisterForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(linkLogin);
+            Controls.Add(btnRegister);
+            Controls.Add(txtEmail);
+            Controls.Add(txtConfirmPassword);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Name = "RegisterForm";
+            Text = "RegisterForm";
+            Load += RegisterForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private TextBox txtConfirmPassword;
+        private TextBox txtEmail;
+        private Button btnRegister;
+        private LinkLabel linkLogin;
+    }
+}
---
+++ Views/RegisterForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan.Views
+{
+    public partial class RegisterForm : Form
+    {
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text;
+            string confirmPassword = txtConfirmPassword.Text;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (password != confirmPassword)
+            {
+                MessageBox.Show("Mật khẩu nhập lại không khớp!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            MessageBox.Show("Đăng ký thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+            this.Close();
+        }
+
+        private void RegisterForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLogin_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+            this.Close();
+        }
+    }
+}
